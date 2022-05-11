adfa4d94f68cc8a0709cb0d8f86392544a82925c
Prince Kunzwa <princekunzwa@outlook.com>
2022-05-12 00:15:22 +0100

    Updated Hash and stacks code

--- DataStructures/HashTables/index.js
+++ DataStructures/HashTables/index.js
@@ -1,86 +1,86 @@
 // Implementing a Hash Table
 
-// Object
-// class HashTable {
-//     // constructor receiving memory space size
-//     constructor(size){
-//         // this data store the size of the space in a new array
-//         // Array instantiation 
-//     this.data = new Array(size);
+Object
+class HashTable {
+    // constructor receiving memory space size
+    constructor(size){
+        // this data store the size of the space in a new array
+        // Array instantiation 
+    this.data = new Array(size);
 
     
 
 
-//     }
+    }
 
-//     // Hash function to generate a hash for the key
-//     _hash(key) {
-//         let hash = 0;
-//         for (let i = 0; i < key.length; i++) {
-//             // charCodeAt gives us a character code at whatever index we declare
-//             hash = (hash + key.charCodeAt(i) * i) %
-//             this.data.length
-//         }
-//         return hash;
-//     }
+    // Hash function to generate a hash for the key
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            // charCodeAt gives us a character code at whatever index we declare
+            hash = (hash + key.charCodeAt(i) * i) %
+            this.data.length
+        }
+        return hash;
+    }
 
-//     // Setting a key value pair
-//     set(key, value) {
-//         let address = this._hash(key);
-//         if(!this.data[address]) {
-//             this.data[address] = [];
-//         }
-//         this.data[address].push([key, value])
-//         return this.data
-//     }
+    // Setting a key value pair
+    set(key, value) {
+        let address = this._hash(key);
+        if(!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value])
+        return this.data
+    }
 
-//     // Get method to receive a key
-//     get(key){
-//         let address = this._hash(key);
-//         const currentBucket = this.data[address];
-//         if(currentBucket){
-//             for(let i = 0; i < currentBucket.length; i++){
-//                 if(currentBucket[i][0] == key){
-//                     return currentBucket[i][0]
-//                 }
-//             }
-//         }
-//         return undefined
-//     }
+    // Get method to receive a key
+    get(key){
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        if(currentBucket){
+            for(let i = 0; i < currentBucket.length; i++){
+                if(currentBucket[i][0] == key){
+                    return currentBucket[i][0]
+                }
+            }
+        }
+        return undefined
+    }
 
-//     keys(){
-//         const keysArray = [];
-//         for (let i=0; i < this.data.length; i++){
-//             if(this.data[i]) {
-//                 keysArray.push(this.data[i][0][0])
-//             }
-//         }
-//         return keysArray;
-//     }
-// }
+    keys(){
+        const keysArray = [];
+        for (let i=0; i < this.data.length; i++){
+            if(this.data[i]) {
+                keysArray.push(this.data[i][0][0])
+            }
+        }
+        return keysArray;
+    }
+}
 
-// const myHashTable = new HashTable(50);
-// myHashTable.set('grapes', 100000);
-// myHashTable.set('oranges', 10);
-// myHashTable.set('apples', 5);
-// myHashTable.keys();
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 100000);
+myHashTable.set('oranges', 10);
+myHashTable.set('apples', 5);
+myHashTable.keys();
 
 
 
 
 // First recurring character
 
-// function firstRecurringCharacter(input) {
-//     for (let i = 0; i < input.length; i++) {
-//         for (let j = i + 1; j < input.length; j++){
-//             if(input[i] === input[j]) {
-//                 return input[i];
-//             }
-//         }
-//     }
-//     return undefined;
-// } // O(n^2)
-// firstRecurringCharacter([]);
+function firstRecurringCharacter(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++){
+            if(input[i] === input[j]) {
+                return input[i];
+            }
+        }
+    }
+    return undefined;
+} // O(n^2)
+firstRecurringCharacter([]);
 
 
 
--- DataStructures/Stacks&Queues/index.js
+++ DataStructures/Stacks&Queues/index.js
@@ -1,59 +1,59 @@
-// class Node {
-//     constructor(value){
-//       this.value = value;
-//       this.next = null;
-//     }
-//   }
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
   
-//   // Stack example
-//   class Stack {
-//     constructor(){
-//       this.top = null;
-//       this.bottom = null;
-//       this.length = 0;
-//     }
-//     // Look at the first titem
-//     peek() {
-//       return this.top;
-//     }
+  // Stack example
+  class Stack {
+    constructor(){
+      this.top = null;
+      this.bottom = null;
+      this.length = 0;
+    }
+    // Look at the first titem
+    peek() {
+      return this.top;
+    }
 
-//     push(value){
-//       const newNode = new Node(value);
-//       if (this.length === 0) {
-//         this.top = newNode;
-//         this.bottom = newNode;
-//       } else {
-//         const holdingPointer = this.top;
-//         this.top = newNode;
-//         this.top.next = holdingPointer;
-//       }
-//       this.length++;
-//       return this;
-//     }
-//     pop(){
-//       if (!this.top) {
-//         return null;
-//       }
-//       if (this.top === this.bottom) {
-//         this.bottom = null;
-//       }
-//       const holdingPointer = this.top;
-//       this.top = this.top.next;
-//       this.length--;
-//       return this;
-//     }
-//     //isEmpty
-//   }
+    push(value){
+      const newNode = new Node(value);
+      if (this.length === 0) {
+        this.top = newNode;
+        this.bottom = newNode;
+      } else {
+        const holdingPointer = this.top;
+        this.top = newNode;
+        this.top.next = holdingPointer;
+      }
+      this.length++;
+      return this;
+    }
+    pop(){
+      if (!this.top) {
+        return null;
+      }
+      if (this.top === this.bottom) {
+        this.bottom = null;
+      }
+      const holdingPointer = this.top;
+      this.top = this.top.next;
+      this.length--;
+      return this;
+    }
+    //isEmpty
+  }
   
-//   const myStack = new Stack();
-//   myStack.peek();
-//   myStack.push('google');
-//   myStack.push('udemy');
-//   myStack.push('discord');
-//   myStack.peek();
-//   myStack.pop();
-//   myStack.pop();
-//   myStack.pop();
+  const myStack = new Stack();
+  myStack.peek();
+  myStack.push('google');
+  myStack.push('udemy');
+  myStack.push('discord');
+  myStack.peek();
+  myStack.pop();
+  myStack.pop();
+  myStack.pop();
 
 //  Queue Implementation 
 class Node {
